4ece92f7c9fc15b4c9990155bac607d2789b7b8c
Robert Jung <robert.jung@tolingo.com>
2011-10-27 11:52:34 +0200

    progress…

--- bot.rb
+++ bot.rb
@@ -1,51 +1,56 @@
 require 'rubygems'
 require 'blather/client'
 
-accepted_users = ["robert.jung@tolingo.com", "goosebuster@gmail.com"]
+accepted_users = ["robert.jung@tolingo.com", "goosebucccster@gmail.com"]
 
 setup 'botolingo@googlemail.com', 'funnybot123', 'talk.google.com', 5222, "./cert"
 when_ready {
-	puts "Connected ! send messages to #{jid.stripped}."
+  puts "Connected ! send messages to #{jid.stripped}."
 }
+disconnected { client.connect }
+
+
+before(:message) { |m| puts m.inspect}
+before(:subscrition) { |m| write_to_stream s.unsubscribe! unless accepted_users.include?(s.from.stripped.to_s); puts "go away!" }
+
+
+
+
 
 subscription :request? do |s|
-    sender = m.from.to_s.split(/\//).first
-	if accepted_users.include? sender
-		write_to_stream s.approve!
-	else
-# do what?
-	end
+  sender = s.from.stripped.to_s
+  if accepted_users.include? sender
+    write_to_stream s.approve!
+  else
+    say s.from, "I'm afraid I don't know you yet, " + sender + ", please introduce yourself to my owner!"
+    write_to_stream s.refuse!
+  end
 end
 
-message :chat? do |m|
-  puts m.inspect
-  false # fall through
-end
+# message :chat? do |m|
+#   puts m.inspect
+#   false # fall through
+# end
 
 message :chat?, :body => 'exit' do |m|
-    say m.from, 'Exiting ...'
-	shutdown
+  say m.from, 'Exiting ...'
+  shutdown
 end
 
 message :chat?, :body => /^time$/ do |m|
-	say m.from, Time.now.to_s
+  say m.from, Time.now.to_s
 end
 
 message :chat?, :body => /^exec .*/ do |m|
-	m.body.match /^exec (.*)/
-	pid = fork {
-		system $1
-		puts $1.to_s # logger
-	}
-	Process.detach(pid)
-	say m.from, "You executed: #{$1}"
+  m.body.match /^exec (.*)/
+  pid = fork {
+    system $1
+    puts $1.to_s # logger
+  }
+  Process.detach(pid)
+  say m.from, "You executed: #{$1}"
 end
 
 message :chat?, :body do |m|
-    sender = m.from.to_s.split(/\//).first
-    unless accepted_users.include? sender
-		say m.from, "I'm afraid I don't know you yet, " + sender
-	else
-		say m.from, "Sorry, don't know what \"#{m.body}\" means!"
-	end
+  say m.from, "Sorry, don't know what \"#{m.body}\" means!"
 end
