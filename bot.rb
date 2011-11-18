1026ea136f73913918966328ab056e393cf91130
Robert Jung <robert.jung@tolingo.com>
2011-11-18 11:05:23 +0100

    Teach dead dog new tricks…

--- bot.rb
+++ bot.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
+require 'cgi'
 require 'blather/client'
+require 'chronic_duration'
 
-accepted_users = ["robert.jung@tolingo.com", "goosebucccster@gmail.com"]
+
+accepted_users = ["robert.jung@tolingo.com", "goosebuster@gmail.com"]
 
 setup 'botolingo@googlemail.com', 'funnybot123', 'talk.google.com', 5222, "./cert"
 when_ready {
@@ -20,6 +23,7 @@ before(:subscrition) { |m| write_to_stream s.unsubscribe! unless accepted_users.
 subscription :request? do |s|
   sender = s.from.stripped.to_s
   if accepted_users.include? sender
+    puts "auto-accepted " + sender
     write_to_stream s.approve!
   else
    # say s.from, "I'm afraid I don't know you yet, " + sender + ", please introduce yourself to my owner!"
@@ -43,8 +47,19 @@ message :chat?, :body => 'exit' do |m|
   shutdown
 end
 
+message :chat?, :body => /^in .*/ do |m|
+  m.body.match /^in (.*)/
+  say m.from, 'Will do sthg in ' + ChronicDuration.output(ChronicDuration.parse($1), :format => :short)
+  shutdown
+end
+
 message :chat?, :body => /^time$/ do |m|
   say m.from, Time.now.to_s
+end
+
+message :chat?, :body => /^old .*$/ do |m|
+  m.body.match /^old (.*)/
+  output = IO.popen("open http://isitold.com/results?url=#{CGI.escape($1)}", "r") { |pipe| output = pipe.read }
 end
 
 message :chat?, :body => /^exec .*/ do |m|
